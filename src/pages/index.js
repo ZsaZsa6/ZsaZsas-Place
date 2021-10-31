4772dc5936e9b52c1c162a51a2c32dfc6f9be2bb
Ronda Cowen <cowenhome5@gmail.com>
2021-10-30 22:27:18 -0500

    add plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,17 @@
 import * as React from "react"
 
-// styles
+const IndexPage = () => {
+    return (
+      <main>
+        <title>Zsa Zsa's Place</title>
+        <br/>
+
+        <h1>Welcome to Zsa Zsa's Place!</h1>
+            <h2>Come on in and explore</h2>
+        <p>Placehold</p>
+      </main>
+    )
+  }
 
 
 export default IndexPage
